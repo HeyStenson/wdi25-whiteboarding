16ac702be57b140c7fd81d1fa03d8e95f0cd68e9
HeyStenson <h.n.stenson@gmail.com>
2015-11-30 10:55:14 -0800

    Create javascript-solutions.js

---
+++ javascript-solutions.js
@@ -0,0 +1,13 @@
+```
+function reverseString(str) {
+
+  var reversedStr = "";
+  var i = str.length - 1;
+  while (i >= 0) {
+    reversedStr += str[i];
+    i --;
+  }
+  return reversedStr;
+  
+};
+```
