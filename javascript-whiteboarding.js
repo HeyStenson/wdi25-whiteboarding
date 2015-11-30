0dd6c20f31cb1e9a60c0ca925abb240b9c4edbe9
HeyStenson <h.n.stenson@gmail.com>
2015-11-29 21:13:53 -0800

    basic outline

---
+++ javascript-whiteboarding.js
@@ -0,0 +1,3 @@
+// Write a function in which the input is a string and the output is the reverse of that string. This function whould take  spaces into account. Do not use the built-in JavaScript reverse method.
+
+// Write 
