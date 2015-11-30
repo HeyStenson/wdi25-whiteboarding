62bceacfe881a7496f9bd2e953868eb588053253
HeyStenson <h.n.stenson@gmail.com>
2015-11-29 21:20:53 -0800

    have four basic questions

--- javascript-whiteboarding.js
+++ javascript-whiteboarding.js
@@ -1,3 +1,13 @@
 // Write a function in which the input is a string and the output is the reverse of that string. This function whould take  spaces into account. Do not use the built-in JavaScript reverse method.
+// Input: "Hello I'm Heather", Output: "rehtaeH m'I olleH"
 
-// Write 
+// Write a function that converts an integer into its Roman numeral equivalent. 
+// Input: 4, Output: "IV" 
+// Input: 21, Output: "XXI"
+
+// Write a function that takes an array as an argument and returns an array that contains the contents of the first array twice.
+// Input: [1,2,3,4, "banana"], Output: [1,2,3,4, "banana", 1,2,3,4, "banana"]
+
+// Write a function that checks if the input is a boolean. If it is, return the boolean; if it's not, return an error message.
+// Input: true, Output: true
+// Input: "banana", Output: "Please give me a boolean" 
